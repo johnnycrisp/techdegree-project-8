94b6a0620d88f1e882ad41d5df5a929bd1062275
Johnny Crisp <johnnycrisp2@gmail.com>
2022-09-27 19:58:51 +0200

    added modal css and html

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,20 @@
 const employeeCard = document.querySelectorAll('.employee-card');
+const cardGrid = document.querySelector('.grid-container');
+const modal = document.querySelector('.modal');
 let cardHTML;
+let modalHTML;
+
+cardGrid.addEventListener('click', (e)=>{
+    if(e.target.parentNode.className === 'employee-card'){
+        modal.classList.add('show-modal');
+
+    }
+});
+
+function displayModal(){
+
+
+}
 
 function fetchData(url){
     return fetch(url)
@@ -8,27 +23,33 @@ function fetchData(url){
 }
 
 function generateHTML(data) {
-
     cardHTML = `
-        
+        <img class="employee-img" src="${data.picture.large}" alt="Picture of ${data.name.first} ${data.name.last}">
+        <div class="employee-data-container">
+            <h3>${data.name.first} ${data.name.last}</h3>
+            <p>${data.email}</p>
+            <p>${data.location.city}</p>
+        </div>`;
+        modalHTML = `
         <img class="employee-img" src="${data.picture.large}" alt="Picture of ${data.name.first} ${data.name.last}">
         <div class="employee-data-container">
             <h3>${data.name.first} ${data.name.last}</h3>
             <p>${data.email}</p>
             <p>${data.location.city}</p>
         </div>
-   
-    `;
-    return cardHTML;
+        <div class="employee-data-container">
+            <p>${data.location.street.number} ${data.location.street.name}, ${data.location.state} ${data.location.postcode}</p>
+            <p>${data.dob.date}</p>
+        </div>`;
 }
 
 fetchData('https://randomuser.me/api/?results=12')
-    .then(data => data.results)
+    .then(data => data = data.results)
     .then(data => {
         for(let i=0; i<data.length; i++){
             generateHTML(data[i]);
             employeeCard[i].innerHTML = cardHTML;
         }
-    });  
+    });   
 
 
